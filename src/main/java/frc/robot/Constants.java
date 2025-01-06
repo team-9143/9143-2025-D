8a1cbd3b06aadff04c42a868a33005f818373c62
Armor Robotics 9143 <robotics@riordanhs.org>
2025-01-05 23:35:55 -0800

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+/*
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  // Constants related to operator input devices like controllers and button mappings.
+  public static class OperatorConstants {}
+  
+  // Constants defining IDs and ports for motors, sensors, and other devices.
+  public static class DeviceConstants {}
+
+  // Constants specifying physical dimensions and characteristics of the robot.
+  public static class PhysicalConstants {}
+
+  // Constants used for autonomous routines, including speeds and trajectory parameters.
+  public static class AutoConstants {}
+
+  // Constants for PID controller tuning values for various subsystems.
+  public static class PIDConstants {}
+
+  // Constants for vision processing, including camera parameters and pipeline indices.
+  public static class VisionConstants {}
+
+  // Constants for motor configurations, such as gear ratios and current limits.
+  public static class MotorConstants {}
+}
